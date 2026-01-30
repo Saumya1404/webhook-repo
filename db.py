618906d2b4d5a7d3e7e386aa1c3c76a7ecb144f5
Saumya1404 <saumya1404@gmail.com>
2026-01-30 18:05:07 +0530

    db client

---
+++ db.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client.github_events
+collection = db.events
